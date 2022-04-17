876b5d17acadfcd791d7e1759016c866a0fa8b38
uday1k <udayravi000@gmail.com>
2022-04-17 23:41:21 +0530

    added Edit JOB

--- routes/company.js
+++ routes/company.js
@@ -28,8 +28,18 @@ const checkCompanyAuth=(req, res, next) => {
 }
 
 
-router.get('/', function (req, res) {
-
+router.get('/myjobs', function (req, res) {
+  res.locals.auth=req.session.auth;
+  res.locals.role=req.session.role;
+  async function asRetriveMyJobs(){
+    await dbo.collection("jobsDetails").aggregate([ { $match : { companyName : req.session.companyName } } ]).toArray(function(err,retrivedValues){
+      if(err) throw err;
+      let myJobValues={}
+      myJobValues.rorj=retrivedValues;
+      res.render("myJobs",myJobValues);
+    })
+  }
+  asRetriveMyJobs();
 })
 
 router.get('/register', function (req, res) {
@@ -187,45 +197,91 @@ router.post('/checkcompanydetails', function (req, res) {
 
 
 
-router.get('/insertjob',checkCompanyAuth, function (req, res) {
+router.get('/insertjob/:idofjob?', function (req, res) {
+    let idofjob=req.params.idofjob;
 
-  let jobDetails = {};
+    const skillsTypes = new Promise(function (resolve, reject) {
+      dbo.collection("skillsTypes").find({}).toArray(function (err, skillTypes) {
+        if (err) reject(err);
+        else resolve(skillTypes);
+      })
 
+    })
+    const roleTypes = new Promise(function (resolve, reject) {
+      dbo.collection("roleTypes").find({}).toArray(function (err, roleTypes) {
+        if (err) reject(err);
+        else resolve(roleTypes);
+      })
 
+    })
+    const deptTypes = new Promise(function (resolve, reject) {
+      dbo.collection("departmentTypes").find({}).toArray(function (err, deptTypes) {
+        if (err) reject(err);
+        else resolve(deptTypes);
+      })
 
-  const skillsTypes = new Promise(function (resolve, reject) {
-    dbo.collection("skillsTypes").find({}).toArray(function (err, skillTypes) {
-      if (err) reject(err);
-      else resolve(skillTypes);
+    })
+    const quals = new Promise(function (resolve, reject) {
+      dbo.collection("Qualifications").find({}).toArray(function (err, qualsres) {
+        if (err) reject(err);
+        else resolve(qualsres);
+      })
     })
 
-  })
-  const roleTypes = new Promise(function (resolve, reject) {
-    dbo.collection("roleTypes").find({}).toArray(function (err, roleTypes) {
-      if (err) reject(err);
-      else resolve(roleTypes);
-    })
+    if(!(idofjob))
+    {
+            let jobDetails = {};
+          
+          async function asFuncEnterJobDetails() {
+            [jobDetails.typesOfSkills, jobDetails.typesOfRoles, jobDetails.typesOfDepts, jobDetails.quals] = await Promise.all([skillsTypes, roleTypes, deptTypes, quals])
+            jobDetails.departmetSelected=null;
+            jobDetails.roleSelected=null;
+            jobDetails.qualificationSelected=null;
+            jobDetails.jobDescriptionGiven=null;
+            jobDetails.skillsSelected=null;
+            jobDetails.experienceGiven=null;
+            jobDetails.lastDateTimeToApplyGiven=null;
+            jobDetails.ctcMentioned=null;
+            jobDetails._id=null;
+            res.render('formpage', jobDetails);
+          }
+          asFuncEnterJobDetails();
+    }
+    else{
 
-  })
-  const deptTypes = new Promise(function (resolve, reject) {
-    dbo.collection("departmentTypes").find({}).toArray(function (err, deptTypes) {
-      if (err) reject(err);
-      else resolve(deptTypes);
-    })
 
-  })
-  const quals = new Promise(function (resolve, reject) {
-    dbo.collection("Qualifications").find({}).toArray(function (err, qualsres) {
-      if (err) reject(err);
-      else resolve(qualsres);
-    })
-  })
+      const jobDetailsofIDPromise = new Promise(function (resolve, reject) {
+        dbo.collection("jobsDetails").findOne({ "_id": ObjectId(idofjob)},function (err, jobres) {
+          if (err) reject(err);
+          else resolve(jobres);
+        })
+      })
 
-  async function asFuncEnterJobDetails() {
-    [jobDetails.typesOfSkills, jobDetails.typesOfRoles, jobDetails.typesOfDepts, jobDetails.quals] = await Promise.all([skillsTypes, roleTypes, deptTypes, quals])
-    res.render('formpage', jobDetails);
-  }
-  asFuncEnterJobDetails();
+      let jobDetails = {};
+
+          
+          async function asFuncUpdateJobDetails() {
+            
+
+            [jobDetails.typesOfSkills, jobDetails.typesOfRoles, jobDetails.typesOfDepts, jobDetails.quals] = await Promise.all([skillsTypes, roleTypes, deptTypes, quals])
+            let jobDetailsofID=await jobDetailsofIDPromise;
+            console.log(jobDetailsofID);
+            jobDetails.departmetSelected=jobDetailsofID.deptType;
+            jobDetails.roleSelected=jobDetailsofID.roleType;
+            jobDetails.qualificationSelected=jobDetailsofID.qualificationRequired;
+            jobDetails.jobDescriptionGiven=jobDetailsofID.jobDescription;
+            jobDetails.skillsSelected=jobDetailsofID.skillsRequired;
+            jobDetails.experienceGiven=jobDetailsofID.experience;
+            let dt=jobDetailsofID.lastDateTimeToApply;
+            let datech=(dt.slice(6,10)+'-'+dt.slice(3,5)+'-'+dt.slice(0,2)+'T'+dt.slice(12,14)+':'+dt.slice(15,17));
+            jobDetails.lastDateTimeToApplyGiven=datech;
+            jobDetails.ctcMentioned=jobDetailsofID.ctcOffered;
+            jobDetails._id=jobDetailsofID._id;
+            res.render('formpage', jobDetails);
+          }
+          asFuncUpdateJobDetails();
+      
+    }
 
 
 })
@@ -234,25 +290,50 @@ router.get('/insertjob',checkCompanyAuth, function (req, res) {
 
 
 router.post('/savejob', function (req, res) {
-  let insertJobDetails = {};
-  insertJobDetails.deptType = req.body.jobdept;
-  insertJobDetails.roleType = req.body.jobrole;
-  insertJobDetails.qualificationRequired = req.body.jobqual;
-  insertJobDetails.jobDescription = req.body.jobdes;
-  insertJobDetails.skillsRequired = req.body.jobskillsrequired;
-  insertJobDetails.experience = req.body.jobexp;
-  insertJobDetails.lastDateTimeToApply = new Date(req.body.jobdatetime).toLocaleString();
-  insertJobDetails.ctcOffered = req.body.jobctc;
-  insertJobDetails.postedDateTime = new Date().toLocaleString();
-  insertJobDetails.companyName=req.session.companyName;
-  async function asFuncInsertJobDetails() {
-    await dbo.collection("jobsDetails").insertOne(insertJobDetails, function (err, resultToCheckDetailsInserted) {
-      if (err) throw err;
-      req.flash('checkFlash','Job Successfully Published');
-      res.redirect("/");
-    })
+  if(!(req.body.jobmongoid)){
+    let insertJobDetails = {};
+    insertJobDetails.deptType = req.body.jobdept;
+    insertJobDetails.roleType = req.body.jobrole;
+    insertJobDetails.qualificationRequired = req.body.jobqual;
+    insertJobDetails.jobDescription = req.body.jobdes;
+    insertJobDetails.skillsRequired = req.body.jobskillsrequired;
+    insertJobDetails.experience = req.body.jobexp;
+    insertJobDetails.lastDateTimeToApply = new Date(req.body.jobdatetime).toLocaleString();
+    insertJobDetails.ctcOffered = req.body.jobctc;
+    insertJobDetails.postedDateTime = new Date().toLocaleString();
+    insertJobDetails.companyName=req.session.companyName;
+    async function asFuncInsertJobDetails() {
+      await dbo.collection("jobsDetails").insertOne(insertJobDetails, function (err, resultToCheckDetailsInserted) {
+        if (err) throw err;
+        req.flash('checkFlash','Job Successfully Published');
+        res.redirect("/");
+      })
+    }
+    asFuncInsertJobDetails();
   }
-  asFuncInsertJobDetails();
+  else{
+    let ObjectContainingIDtobeEffected = { "_id": ObjectId(req.body.jobmongoid) };
+    let newvalues={};
+    newvalues = { $set: {   deptType: req.body.jobdept,roleType : req.body.jobrole,qualificationRequired : req.body.jobqual
+                                ,jobDescription : req.body.jobdes
+                                ,skillsRequired : req.body.jobskillsrequired
+                                ,experience : req.body.jobexp
+                                ,lastDateTimeToApply : new Date(req.body.jobdatetime).toLocaleString()
+                                ,ctcOffered : req.body.jobctc
+                              }
+                  }
+    
+                  async function asFuncUpdateJobDetails() {
+                    await  dbo.collection("jobsDetails").updateOne(ObjectContainingIDtobeEffected,newvalues,function (err, resultToCheckDetailsInserted) {
+                      if (err) throw err;
+                      req.flash('checkFlash','Job Successfully Published');
+                      res.redirect("/");
+                    })
+                  }
+                  asFuncUpdateJobDetails();
+
+  }
+  
 
 })
 
